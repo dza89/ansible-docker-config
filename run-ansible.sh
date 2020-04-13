ff1ef07fd66f3e6186360681693e516b2770385a
daan <dhapotter@gmail.com>
2020-04-13 21:08:05 +0200

    fix ssh

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,3 +1,1 @@
-docker run --rm -it -v $(pwd)/ansible:/ansible \
-            -v ~/.ssh/id_rsa:/root/id_rsa \
-            willhallonline/ansible:2.9-alpine ansible-playbook playbook.yml
+docker run --rm -it --network host -v $(pwd)/ansible:/ansible -v ~/.ssh/id_rsa:/root/id_rsa willhallonline/ansible:2.9-alpine ansible-playbook /ansible/playbook.yml
