63006a601013e8d4411585be20082f9dfebd4ede
daan <dhapotter@gmail.com>
2020-04-19 20:12:53 +0200

    remove docker compose

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,1 +1,7 @@
-docker run --rm -it --network host -v $(pwd)/ansible:/ansible -v ~/.ssh/id_rsa:/root/id_rsa willhallonline/ansible:2.9-alpine -i ansible/hosts /ansible/playbook.yml
+docker run --rm -it --network host \
+    -w /ansible \
+    -v $(pwd)/ansible:/ansible \
+    willhallonline/ansible:2.9-alpine \
+            --extra-vars upassword=${UPASSWORD} uusername=${UUSERNAME} ansible_ssh_pass=${ssh_pass} \
+                         ansible_ssh_host=${MYIP}
+            playbook.yml 
