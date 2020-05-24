92aa011f32e2bf1c28205f39a20c8612910fd653
daan <daan.potter@nn-group.com>
2020-05-24 16:33:42 +0200

    make ansible great again

--- run-ansible.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm -it --network host \
-    -w /ansible \
-    -v $(pwd)/ansible:/ansible \
-    willhallonline/ansible:2.9-alpine \
-            --extra-vars upassword=${UPASSWORD} uusername=${UUSERNAME} ansible_ssh_pass=${ssh_pass} \
-                         ansible_ssh_host=${MYIP}
-            playbook.yml 
